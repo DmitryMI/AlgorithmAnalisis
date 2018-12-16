19db445e8c2210d031d10bb03989fe122af5194b
DimKaHUI <wet75787@tqosi.com>
2018-12-16 19:10:45 +0300

    Report completed

--- Lab07_Conveyer/Conveyer/Program.cs
+++ Lab07_Conveyer/Conveyer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,57 +15,92 @@ namespace ConveyerLab
 
         static void Main(string[] args)
         {
-            Conveyer.Job[] jobs = new Conveyer.Job[]
-            {
-                Incrementer, Incrementer, Incrementer
-            };
-
-            Conveyer conveyer = new Conveyer(jobs);
-
-            conveyer.WriteNext(1);
-            conveyer.WriteNext(2);
-            conveyer.WriteNext(3);
-
-            while (true)
-            {
-                if (conveyer.HasNextResult())
-                {
-                    int result = (int) conveyer.ReadNextResult();
-                    Console.WriteLine("Result 1: " + result);
-                    break;
-                }
-            }
-
-            while (true)
-            {
-                if (conveyer.HasNextResult())
-                {
-                    int result = (int)conveyer.ReadNextResult();
-                    Console.WriteLine("Result 2: " + result);
-                    break;
-                }
-            }
-
-            while (true)
-            {
-                if (conveyer.HasNextResult())
-                {
-                    int result = (int)conveyer.ReadNextResult();
-                    Console.WriteLine("Result 3: " + result);
-                    break;
-                }
-            }
-
-            conveyer.StopConveyer();
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Experiment();
         }
 
         static object Incrementer(object val)
         {
             Thread.Sleep(_rnd.Next(1, 5) * 1000);
             return (int) val + 1;
+        }
+
+        static object FastIncrementer(object val)
+        {
+            Thread.Sleep(1000);
+            return (int)val + 1;
+        }
+
+        static object MediumIncrementer(object val)
+        {
+            Thread.Sleep(3000);
+            return (int)val + 1;
+        }
+
+        static object SlowIncrementer(object val)
+        {
+            Thread.Sleep(5000);
+            return (int)val + 1;
+        }
+
+        static void Experiment()
+        {
+            DateTime start = DateTime.Now;
+            DoInConveyer();
+
+            TimeSpan span = DateTime.Now - start;
+
+            Console.WriteLine("Conveyer time: " + span);
+
+            start = DateTime.Now;
+            DoLinearly();
+            span = DateTime.Now - start;
+
+            Console.WriteLine("Linear time: " + span);
+
+            Console.ReadKey();
+        }
+
+        static void DoLinearly()
+        {
+            int[] init = new int[]{1, 2, 3,4, 5, 6, 7, 8, 9};
+            for (int i = 0; i < init.Length; i++)
+            {
+                int result = (int)FastIncrementer(MediumIncrementer(SlowIncrementer(init[i])));
+
+                Console.WriteLine("Result " + (i + 1) + ": " + result);
+            }
+        }
+
+        static void DoInConveyer()
+        {
+            Conveyer.Job[] jobs = new Conveyer.Job[]
+            {
+                FastIncrementer, MediumIncrementer, SlowIncrementer
+            };
+
+            Conveyer conveyer = new Conveyer(jobs);
+
+            int[] init = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < init.Length; i++)
+            {
+                conveyer.WriteNext(init[i]);
+            }
+
+            for (int i = 0; i < init.Length; i++)
+            {
+                while (true)
+                {
+                    if (conveyer.HasNextResult())
+                    {
+                        int result = (int) conveyer.ReadNextResult();
+                        Console.WriteLine("Result " + (i + 1) + ": " + result);
+                        break;
+                    }
+                }
+            }
+
+            conveyer.StopConveyer();
         }
     }
 }
